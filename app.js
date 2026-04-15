f4dd59a9026217232bb545f3106fea295115c928
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-04-15 23:40:28 +0200

    Added middleware

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts')
 
+function pageNotFound(req, res, next) {
+    res.status(404)
+    res.json({
+        error: "Not Found any Page",
+        message: "Pagina non trovata"
+    })
+    next();
+}
+
+function errorsHandler(err, req, res, next) {
+    res.status(500)
+    res.json({
+        error: err.message
+    });
+
+}
+
 app.use(express.static('public'));
 app.use(express.json());
+app.use(pageNotFound)
+app.use(errorsHandler)
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
